0f5cb63b05c658e3543e16397d857ec988463c94
Brayan Talavera <brayantalavera@macbook-pro-de-brayan.local>
2016-07-18 00:24:35 -0500

    Initial Commit

---
+++ pruebaGit/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  pruebaGit
+//
+//  Created by Brayan Talavera on 18/07/16.
+//  Copyright © 2016 Brayan Talavera. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
